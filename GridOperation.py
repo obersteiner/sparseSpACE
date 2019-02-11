e82cef5f5a21fed4f8365a1cc79ac132fb009aaa
Obersteiner <oberstei@in.tum.de>
2019-02-11 14:09:55 +0100

    added first template for GridOperation class

---
+++ GridOperation.py
@@ -0,0 +1,34 @@
+import abc
+
+
+class GridOperation(object):
+    def is_area_operation(self):
+        return False
+    @abc.abstractmethod
+    def perform_operation(self, levelvector, refinementObjects=None):
+        pass
+
+    @abc.abstractmethod
+    def set_errors(self, refinementObjects=None):
+        pass
+
+
+class GridOperationArea(GridOperation):
+    def is_area_operation(self):
+        return True
+
+    def perform_operation(self, levelvector, refinementObjects=None):
+        for area in enumerate(refinementObjects):
+            self.evaluate_area(area, levelvector)
+
+    @abc.abstractmethod
+    def evaluate_area(self, area, levelvector):
+        pass
+
+
+class Integration(GridOperationArea):
+    def __init__(self, f):
+        self.f = f
+
+    def evaluate_area(self, area, levelvector):
+        pass
