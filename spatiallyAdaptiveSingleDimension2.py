3dfff98fc51d69b6d91c122e30eee3ea9a44b91a
Michael Obersteiner <oberstei@in.tum.de>
2019-05-22 14:59:33 +0200

    increased error range in which refinement happen

--- spatiallyAdaptiveSingleDimension2.py
+++ spatiallyAdaptiveSingleDimension2.py
@@ -27,7 +27,7 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
         self.dict_points = {}
         self.no_previous_integrals = True
         self.use_local_children = self.version == 2
-        self.margin = 1-10**-14 if self.use_local_children else 0.9
+        self.margin = 1-10**-12 if self.use_local_children else 0.9
 
     def interpolate_points(self, interpolation_points, component_grid):
         gridPointCoordsAsStripes, children_indices = self.get_point_coord_for_each_dim(component_grid.levelvector)
