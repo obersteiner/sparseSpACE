Initialize the grid points using the PPF and fix volume calculation
Also, add missing self.compute_1D_quad_weights argument

GridOperation.py
@@ -365,7 +365,8 @@ class Integration(AreaOperation):
                 left_parent = child_info.left_parent
                 right_parent = child_info.right_parent
                 child = child_info.child
-                volume, evaluations = self.sum_up_volumes_for_point(left_parent=left_parent, right_parent=right_parent, child=child, grid_points=grid_points, d=d)
+                level_child = child_info.level_child
+                volume, evaluations = self.sum_up_volumes_for_point(left_parent=left_parent, right_parent=right_parent, child=child, level_child=level_child, grid_points=grid_points, d=d)
                 k_old = 0
                 for i in range(refinement_dim.size() ):
                     if refinement_dim.get_object(i).start >= left_parent * (1 - tol):
@@ -428,8 +429,7 @@ class Integration(AreaOperation):
 
     # Sum up the 1-d surplusses along the dim-1 dimensional slice through the point child in dimension d.
     #  The surplusses are calculated based on the left and right parents.
-    def sum_up_volumes_for_point(self, left_parent, right_parent, child, grid_points, d):
-        #print(grid_points)
+    def sum_up_volumes_for_point(self, left_parent, right_parent, child, level_child, grid_points, d):
         volume = 0.0
         assert right_parent > child > left_parent
 
@@ -440,8 +440,9 @@ class Integration(AreaOperation):
                 left_parent = p
             if isclose(p, right_parent):
                 right_parent = p
-        index_right_parent = grid_points[d].index(right_parent) - 1 * int(not self.grid.boundary)
         index_left_parent = grid_points[d].index(left_parent) - 1 * int(not self.grid.boundary)
+        index_child = grid_points[d].index(child) - 1 * int(not self.grid.boundary)
+        index_right_parent = grid_points[d].index(right_parent) - 1 * int(not self.grid.boundary)
 
         left_parent_in_grid = self.grid.boundary or not(isclose(left_parent, self.a[d]))
         right_parent_in_grid = self.grid.boundary or not(isclose(right_parent, self.b[d]))
@@ -452,6 +453,7 @@ class Integration(AreaOperation):
             points_right_parent = list(zip(*[g.ravel() for g in np.meshgrid(*[self.grid_surplusses.coords[d2] if d != d2 else [right_parent] for d2 in range(self.dim)])]))
         points_children = list(zip(*[g.ravel() for g in np.meshgrid(*[self.grid_surplusses.coords[d2] if d != d2 else [child] for d2 in range(self.dim)])]))
         indices = list(zip(*[g.ravel() for g in np.meshgrid(*[range(len(self.grid_surplusses.coords[d2])) if d != d2 else None for d2 in range(self.dim)])]))
+        weight_child = self.grid_surplusses.weights[d][index_child]
         for i in range(len(points_children)):
             index = indices[i]
             factor = np.prod([self.grid_surplusses.weights[d2][index[d2]] if d2 != d else 1 for d2 in range(self.dim)])
@@ -501,8 +503,7 @@ class Integration(AreaOperation):
                     else:
                         assert points_right_parent[i] in self.f.f_dict
                         value -= 0.5 * self.f(points_right_parent[i])
-                # ~ volume += factor * abs(value) * (right_parent - child)**exponent
-                volume += factor * abs(value) * (right_parent - left_parent) * 0.5
+                volume += factor * abs(value) * (weight_child * 2 ** -level_child)**exponent
         if self.version == 0 or self.version == 2:
             evaluations = len(points_children) #* (1 + int(left_parent_in_grid) + int(right_parent_in_grid))
         else:

spatiallyAdaptiveSingleDimension2.py
@@ -38,7 +38,7 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
         else:
             self.margin = margin
         self.operation = operation
-        self.equidistant = True
+        self.equidistant = not isinstance(operation, UncertaintyQuantification)
 
     def interpolate_points(self, interpolation_points, component_grid):
         gridPointCoordsAsStripes, _ = self.get_point_coord_for_each_dim(component_grid.levelvector)
@@ -148,7 +148,7 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
         level_child = level_dim[position]
         if self.equidistant:
             width = (self.b[d] - self.a[d]) / 2**level_child
-            return NodeInfo(child, child - width, child + width, True, True, None,None)
+            return NodeInfo(child, child - width, child + width, True, True, None,None, level_child)
         else:
             left_parent = None
             for i in reversed(range(position)):
@@ -162,7 +162,7 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
                     right_parent = coords_dim[i]
                     break
             assert right_parent is not None
-            return NodeInfo(child, left_parent, right_parent, True, True, None,None)
+            return NodeInfo(child, left_parent, right_parent, True, True, None,None, level_child)
 
     # this method draws the 1D refinement of each dimension individually
     def draw_refinement(self, filename=None, markersize=10):  # update with meta container
@@ -229,21 +229,23 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
 
     def initialize_refinement(self):
         initial_points = []
+
         for d in range(self.dim):
-            initial_points.append(np.linspace(self.a[d], self.b[d], 2 ** 2 + 1))
+            points = [self.a[d], None, None, None, self.b[d]]
+            points[2] = self.grid.get_mid_point(points[0], points[4], d)
+            points[1] = self.grid.get_mid_point(points[0], points[2], d)
+            points[3] = self.grid.get_mid_point(points[2], points[4], d)
+            initial_points.append(np.array(points))
         levels = [0, 2, 1, 2, 0]
 
-        uq_operation = None
-        if isinstance(self.operation, UncertaintyQuantification):
-            uq_operation = self.operation
         containers = []
         for d in range(self.dim):
             objects = []
             for i in range(2 ** 2):
                 objects.append(RefinementObjectSingleDimension(
                     initial_points[d][i], initial_points[d][i + 1], d, self.dim,
-                    (levels[i], levels[i+1]), self.lmax[d] - 2,
-                    dim_adaptive=self.dim_adaptive, uq_operation=uq_operation))
+                    (levels[i], levels[i+1]), self.grid, self.lmax[d] - 2,
+                    dim_adaptive=self.dim_adaptive))
             containers.append(RefinementContainer(objects, d,
                 self.errorEstimator))
         self.refinement = MetaRefinementContainer(containers)
@@ -298,7 +300,7 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
 
 
 class NodeInfo(object):
-    def __init__(self, child, left_parent, right_parent, has_left_child, has_right_child, left_refinement_object, right_refinement_object):
+    def __init__(self, child, left_parent, right_parent, has_left_child, has_right_child, left_refinement_object, right_refinement_object, level_child):
         self.child = child
         self.left_parent = left_parent
         self.right_parent = right_parent
@@ -306,4 +308,5 @@ class NodeInfo(object):
         self.has_right_child = has_right_child
         self.left_refinement_object = left_refinement_object
         self.right_refinement_object = right_refinement_object
+        self.level_child = level_child
 