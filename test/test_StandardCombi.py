8fc91096122a2ddf222ed2da404cf8ca10f90cec
Michael Obersteiner <oberstei@in.tum.de>
2019-08-03 01:05:36 +0200

    new test cases + fix of Grid due to new class structure

--- test/test_StandardCombi.py
+++ test/test_StandardCombi.py
@@ -32,12 +32,12 @@ class TestStandardCombi(unittest.TestCase):
 
     def test_interpolation(self):
         a = -3
-        b = 6
+        b = 7
         for d in range(2, 5):
             standardCombi = StandardCombi(np.ones(d)*a, np.ones(d)*b, grid=TrapezoidalGrid(np.ones(d)*a, np.ones(d)*b, d), print_output=False)
             for l in range(8 - d):
                 for l2 in range(l+1):
-                    f = FunctionLinear([10**i for i in range(d)])
+                    f = FunctionLinear([10*i for i in range(d)])
                     standardCombi.set_combi_parameters(l2, l, f)
                     grid_coordinates = [np.linspace(a, b, 10) for _ in range(d)]
                     interpolated_points = standardCombi.interpolate_grid(grid_coordinates)
@@ -45,12 +45,15 @@ class TestStandardCombi(unittest.TestCase):
                     for component_grid in standardCombi.scheme:
                         interpolated_points_grid = standardCombi.interpolate_points(grid_points, component_grid)
                         for i, p in enumerate(grid_points):
-                            self.assertAlmostEqual((f(p)[0] - interpolated_points_grid[i][0]) / (abs(f(p)[0]) + 10 ** 16), 0)
+                            factor = abs(f(p)[0] if f(p)[0] != 0 else 1)
+                            self.assertAlmostEqual((f(p)[0] - interpolated_points_grid[i][0]) / factor, 0, 13)
                     for i, p in enumerate(grid_points):
-                        self.assertAlmostEqual((f(p)[0] - interpolated_points[i][0])/(abs(f(p)[0]) + 10**16), 0)
+                        factor = abs(f(p)[0] if f(p)[0] != 0 else 1)
+                        self.assertAlmostEqual((f(p)[0] - interpolated_points[i][0])/factor, 0, 13)
                     interpolated_points = standardCombi(grid_points)
                     for i, p in enumerate(grid_points):
-                        self.assertAlmostEqual((f(p)[0] - interpolated_points[i][0])/(abs(f(p)[0]) + 10**16), 0)
+                        factor = abs(f(p)[0] if f(p)[0] != 0 else 1)
+                        self.assertAlmostEqual((f(p)[0] - interpolated_points[i][0])/factor, 0, 13)
 
     def test_number_of_points(self):
         a = -3
