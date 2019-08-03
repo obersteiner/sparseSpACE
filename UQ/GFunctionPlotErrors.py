ad137e16514aba95281f28447a3c2defc43efdb3
FHof <ga84muv@mytum.de>
2019-08-03 14:39:20 +0200

    Update G function test

--- UQ/GFunctionPlotErrors.py
+++ UQ/GFunctionPlotErrors.py
@@ -8,35 +8,30 @@ results_path = tmpdir + "/uqtestG.npy"
 assert os.path.isfile(results_path)
 solutions_data = list(np.load(results_path, allow_pickle=True))
 
-data_spars = []
-data_spars_ho = []
-data_cp = []
+typ_descs = ("full grid Gauß", "Trapez", "HighOrder")
+
+datas = [[] for _ in typ_descs]
 for v in solutions_data:
-	(num_evals, use_spatially_adaptive, do_HighOrder, err_E, err_Var) = v
-	if use_spatially_adaptive:
-		if do_HighOrder:
-			data_spars_ho.append((num_evals, err_E, err_Var))
-			continue
-		data_spars.append((num_evals, err_E, err_Var))
-		continue
-	data_cp.append((num_evals, err_E, err_Var))
-# data_* should be sorted here
-data_spars = np.array(data_spars).T
-data_spars_ho = np.array(data_spars_ho).T
-data_cp = np.array(data_cp).T
+	(num_evals, typid, err_E, err_Var) = v
+	typid = int(typid)
+	datas[typid].append((num_evals, err_E, err_Var))
+
+# datas should be sorted here
+for typid in range(len(typ_descs)):
+	datas[typid] = np.array(datas[typid]).T
 
 figure = plotter.figure(1, figsize=(15,10))
 figure.canvas.set_window_title('Stocha')
 
 for i,desc in enumerate(("E", "Var")):
 	plotter.subplot(2, 1, 1 + i)
-	plotter.plot(data_spars[0], data_spars[i + 1], label=f"adaptive {desc}")
-	plotter.plot(data_spars_ho[0], data_spars_ho[i + 1], label=f"adaptive HO {desc}")
-	plotter.plot(data_cp[0], data_cp[i + 1], label=f"CC {desc}")
+	for typid, typdesc in enumerate(typ_descs):
+		plotter.plot(datas[typid][0], datas[typid][i + 1], "o", label=typdesc)
+		# ~ plotter.plot(datas[typid][0], datas[typid][i + 1], label=typdesc)
 	plotter.xlabel('function evaluations')
-	plotter.ylabel('relative error')
+	plotter.ylabel(f"{desc} relative error")
 	plotter.yscale("log")
-	plotter.legend(loc=2)
+	plotter.legend(loc="lower left")
 	plotter.grid(True)
 
 
--- UQ/TestGFunction.py
+++ UQ/TestGFunction.py
@@ -10,22 +10,29 @@ from ErrorCalculator import *
 from GridOperation import *
 
 
-def run_test(evals_num, use_spatially_adaptive, do_HighOrder):
+types = ((0, "Gauss"), (1, "adaptiveTrapez"), (2, "adaptiveHO"))
+
+d = 2
+a = np.zeros(d)
+b = np.ones(d)
+f_g = FunctionG(d)
+reference_expectation = f_g.get_expectation()
+reference_variance = f_g.get_variance()
+# Create the operation only once
+op = UncertaintyQuantification(None, "Uniform", a, b, dim=d)
+
+def run_test(evals_num, typ, exceed_evals=None):
 	lmax = 2
-	d = 2
-	a = np.zeros(d)
-	b = np.ones(d)
-	f = FunctionG(d)
-	reference_expectation = f.get_expectation()
-	reference_variance = f.get_variance()
+	f = FunctionCustom(lambda x: f_g(x))
+	op.f = f
 
-	op = UncertaintyQuantification(f, "Uniform", a, b, dim=d)
-
-	if use_spatially_adaptive:
-		if do_HighOrder:
-			grid = GlobalHighOrderGridWeighted(a, b, op, boundary=True, modified_basis=False)
-		else:
+	if typ != "Gauss":
+		if typ == "adaptiveHO":
+			grid = GlobalHighOrderGridWeighted(a, b, op, boundary=True)
+		elif typ == "adaptiveTrapez":
 			grid = GlobalTrapezoidalGridWeighted(a, b, op, boundary=True)
+		# ~ elif typ == "BSpline":
+			# ~ grid = GlobalBSplineGrid(a, b)
 		combiinstance = SpatiallyAdaptiveSingleDimensions2(a, b, operation=op,
 			norm=2, grid=grid)
 
@@ -33,16 +40,22 @@ def run_test(evals_num, use_spatially_adaptive, do_HighOrder):
 		expectation_var_func = op.get_expectation_variance_Function()
 		mom2 = reference_variance + reference_expectation * reference_expectation
 		reference_solution = np.array([reference_expectation, mom2])
-		combiinstance.performSpatiallyAdaptiv(1, lmax, expectation_var_func,
-			error_operator, tol=0,
-			max_evaluations=evals_num, reference_solution=reference_solution,
-			print_output=1)
+		if exceed_evals is None:
+			combiinstance.performSpatiallyAdaptiv(1, lmax, expectation_var_func,
+				error_operator, tol=0,
+				max_evaluations=evals_num, reference_solution=reference_solution,
+				print_output=False)
+		else:
+			combiinstance.performSpatiallyAdaptiv(1, lmax, expectation_var_func,
+				error_operator, tol=np.inf,
+				max_evaluations=np.inf, min_evaluations=exceed_evals+1, reference_solution=reference_solution,
+				print_output=False)
 
 		(E,), (Var,) = op.calculate_expectation_and_variance(combiinstance)
 	else:
 		joint_distr = op.distributions_joint
 		nodes, weights = cp.generate_quadrature(
-			evals_num, joint_distr, sparse=True, growth=True)
+			evals_num, joint_distr, rule="G")
 		evals = [f(x)[0] for x in nodes.T]
 		E = sum([v * weights[i] for i,v in enumerate(evals)])
 		mom2 = sum([v * v * weights[i] for i,v in enumerate(evals)])
@@ -57,37 +70,35 @@ def run_test(evals_num, use_spatially_adaptive, do_HighOrder):
 
 	print("evals, relative errors:", num_evals, err_E, err_Var)
 
-	result_data = (num_evals, use_spatially_adaptive, do_HighOrder, err_E, err_Var)
+	typid = -1
+	for t in types:
+		if t[1] == typ:
+			typid = t[0]
+			break
+
+	result_data = (num_evals, typid, err_E, err_Var)
 
 	tmpdir = os.getenv("XDG_RUNTIME_DIR")
 	results_path = tmpdir + "/uqtestG.npy"
 	solutions_data = []
 	if os.path.isfile(results_path):
 		solutions_data = list(np.load(results_path, allow_pickle=True))
-	if all([any([d[i] != result_data[i] for i in range(3)]) for d in solutions_data]):
+	if all([any([d[i] != result_data[i] for i in range(2)]) for d in solutions_data]):
 		solutions_data.append(result_data)
 		np.save(results_path, solutions_data)
 
+	return num_evals
 
-print("Calculating adaptive sparse grid errors for small max evals")
-for i in range(1, 10):
-	evals_num = 19 + i ** 2
-	print("max evals: ", evals_num)
-	run_test(evals_num, True, False)
-	run_test(evals_num, True, True)
 
-print("Calculating adaptive sparse grid errors for big max evals")
-for i in range(6, 10):
-	evals_num = 2 ** i
-	print("max evals: ", evals_num)
-	run_test(evals_num, True, False)
-	run_test(evals_num, True, True)
-# Number of points for order 8 CC
-run_test(1280, True, False)
-run_test(1280, True, True)
+for _,typ in types[1:]:
+	print("Calculations for", typ)
+	evals_num = run_test(1, typ)
+	while evals_num < 1280:
+		print("last evals:", evals_num)
+		evals_num = run_test(None, typ, exceed_evals=evals_num)
 
-print("Calculating Clenshaw-Curtis sparse grid errors")
-for i in range(1, 9):
-	print("CC order: ", i)
-	run_test(i, False, None)
+print("Calculating convent. errors")
+for i in range(1, 36):
+	print("order: ", i)
+	run_test(i, "Gauss")
 
