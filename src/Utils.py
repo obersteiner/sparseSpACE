51182bf814f36e9b4fd85ca718f9ee36d75a2e57
Michael Obersteiner <oberstei@in.tum.de>
2019-08-03 13:21:42 +0200

    replaced numpy routine by better performing itertools routine

--- src/Utils.py
+++ src/Utils.py
@@ -1,11 +1,12 @@
 import numpy as np
 from typing import List, Set, Dict, Tuple, Optional, Union, Sequence
-
+from itertools import product
 
 def get_cross_product(one_d_arrays: Sequence[Sequence[Union[float, int]]]) -> List[Tuple[Union[float, int], ...]]:
-    dim = len(one_d_arrays)
-    return list(zip(*[g.ravel() for g in
-              np.meshgrid(*[one_d_arrays[d] for d in range(dim)], indexing="ij")]))
+    #dim = len(one_d_arrays)
+    #return list(zip(*[g.ravel() for g in
+    #          np.meshgrid(*[one_d_arrays[d] for d in range(dim)], indexing="ij")]))
+    return list(product(*one_d_arrays))
 
 
 def get_cross_product_range(one_d_arrays: Sequence[Sequence[int]]) -> List[Tuple[int, ...]]:
