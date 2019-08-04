36846cd032b0ca57bbb68d88d8299632b28b4b3f
Michael Obersteiner <oberstei@in.tum.de>
2019-08-04 12:10:34 +0200

    improved Hierarchization to reduce time

--- src/Hierarchization.py
+++ src/Hierarchization.py
@@ -2,12 +2,13 @@ import numpy as np
 from Utils import *
 import Grid
 from typing import Tuple, Sequence, Callable
+from Function import *
 
 class HierarchizationLSG(object):
     def __init__(self, grid):
         self.grid = grid
 
-    def __call__(self, f: Callable[[Tuple[float, ...]], Sequence[float]], numPoints: Sequence[int], grid: Grid) -> Sequence[Sequence[float]]:
+    def __call__(self, f: Function, numPoints: Sequence[int], grid: Grid) -> Sequence[Sequence[float]]:
         self.grid = grid
         self.dim = len(numPoints)
         #grid values to be filled with hierarchical surplusses
@@ -21,46 +22,52 @@ class HierarchizationLSG(object):
 
     # this function applies a one dimensional hierarchization (in dimension d) to the array grid_values with
     # numPoints (array) many points for each dimension
-    def hierarchize_poles_for_dim(self, grid_values: Sequence[Sequence[float]], numPoints: Sequence[int], f: Callable[[Tuple[float, ...]], Sequence[float]], d: int, first_dimension: bool) -> Sequence[Sequence[float]]:
+    def hierarchize_poles_for_dim(self, grid_values: Sequence[Sequence[float]], numPoints: Sequence[int], f: Function, d: int, first_dimension: bool) -> Sequence[Sequence[float]]:
         self.dim = len(numPoints)
         offsets = np.array([int(np.prod(numPoints[d+1:])) for d in range(self.dim)])
         numPoints_slice = np.array(numPoints)
         numPoints_slice[d] = 1
         # create all indeces in d-1 dimensional slice
         point_indeces = get_cross_product_range(numPoints_slice)
+        # in the first dimension we need to fill it with the actual function values
+        if first_dimension:
+            points, _ = self.grid.get_points_and_weights()
+            for i, point in enumerate(points):
+                grid_values[:, i] = f(point)
+        # create and fill matrix for linear system of equations
+        # evaluate all basis functions at all grid points
+        matrix = np.empty((numPoints[d], numPoints[d]))
+        for i in range(numPoints[d]):
+            for j in range(numPoints[d]):
+                matrix[i, j] = self.grid.get_basis(d, j)(self.grid.get_coordinates_dim(d)[i])
+
+        pole_coordinates_base = np.empty(numPoints[d], dtype=int)
+        for i in range(numPoints[d]):
+            pole_index = np.zeros(self.dim, dtype=int)
+            pole_index[d] = i
+            pole_coordinates_base[i] = self.get_1D_coordinate(pole_index, offsets)
+
         # iterate over all indeces in slice (0 at dimension d)
         for point_index in point_indeces:
             # create array of function values through pole
             pole_values = np.zeros((f.output_length(), numPoints[d]))
-            pole_positions = []
+            #print(pole_coordinates_base)
+            pole_coordinates = pole_coordinates_base + int(self.get_1D_coordinate(np.asarray(point_index), offsets))
+            #print(pole_coordinates)
             # fill pole_values with function or surplus values of pole through current index
             for i in range(numPoints[d]):
-                pole_index = np.asarray(point_index[:d] + (i,) + point_index[d+1:])
-                position = np.empty(self.dim)
-                for dim in range(self.dim):
-                    position[dim] = self.grid.get_coordinates_dim(dim)[pole_index[dim]]
-                pole_positions.append(position)
-                # in the first dimension we need to fill it with the actual function values
-                if first_dimension:
-                    pole_values[:, i] += f(position)
-                else:
-                    # use previous surplusses for every consecutive dimension (unidirectional principle)
-                    pole_values[:, i] = grid_values[:, self.get_1D_coordinate(pole_index, offsets)]
-            # create and fill matrix for linear system of equations
-            # evaluate all basis functions at all grid points
-            matrix = np.empty((numPoints[d], numPoints[d]))
-            for i in range(numPoints[d]):
-                for j in range(numPoints[d]):
-                    matrix[i, j] = self.grid.get_basis(d, j)(pole_positions[i][d])
+                # use previous surplusses for every consecutive dimension (unidirectional principle)
+                pole_values[:, i] = grid_values[:, pole_coordinates[i]]
+
             # solve system of linear equations for all components of our function values (typically scalar -> output.length = 1)
             # if the function outputs vectors then we have to iterate over all components individually
-            # toDo replace by LU factorization to save time if output.lenth > 1
+            # toDo replace by LU factorization to save time
             #(matrix, self.grid.get_coordinates_dim(d), d)
             for n in range(f.output_length()):
                 hierarchized_values = np.linalg.solve(matrix, pole_values[n,:])
                 for i in range(numPoints[d]):
-                    pole_index = point_index[:d] + (i,) + point_index[d+1:]
-                    grid_values[n,self.get_1D_coordinate(pole_index, offsets)] = hierarchized_values[i]
+                    #pole_index = point_index[:d] + (i,) + point_index[d+1:]
+                    grid_values[n,pole_coordinates[i]] = hierarchized_values[i]
         return grid_values
 
     # this function maps the d-dimensional index to a one-dimensional array index
