319b9c1a9bd92a20ef86f36f535c2bb3ca40c6f5
FHof <ga84muv@mytum.de>
2019-08-27 16:41:11 +0200

    PredatorPrey: faster singlestep test

--- UQ/TestPredatorPreySinglestep.py
+++ UQ/TestPredatorPreySinglestep.py
@@ -1,19 +1,11 @@
-'''
-Created on 27.02.2015
-
-Uncertainty quantification with the Stochastic collocation approach for a predator & prey model (Lotka & Voltera)
-
-@author: Florian Kuenzner
-'''
-
 import chaospy as cp
 import numpy as np
-import scipy.integrate as ode
-import matplotlib.pyplot as plotter
 import sys
 import time
 import os
-from math import isclose
+from math import isclose, isinf
+
+import PredatorPreyCommon as pp
 
 # Load spatially adaptive sparse grid related files
 sys.path.append('../src/')
@@ -29,28 +21,14 @@ uniform_distr = False
 verbose = False
 
 
-#predator = coyote
-#prey = sheep
-
-#initial parameters: sheep/coyote model!!
-coyoteDeathRate = 0.0005 #death rate of coyote
-sheepBirthRate = 0.005 #birth rate of sheep
 voracity = 0.00012 #the voracity rate (when a predator meets sheep and kill it) (Gefraessigkeit)
-augmentation = 0.002*voracity #the augmentation rate (when a coyote meets sheep and a new coyote growth) (Vermehrung)
-
 sheeps_Px0 = 2000 #initial population size of sheep population
 coyote_Px0 = 50 #initial population size of coyote population
-
-T = 70*365 # end of simulation
-NT = int(0.01 * T)  # number of time steps
 
 # Standard deviations
 sigma_voracity = 0.000002  # no uncertainty: 0.000000001, uncertainty: 0.000001
 sigma_sheeps_Px0 = 1
 sigma_coyote_Px0 = 5
-
-# Maximum PCE polynomial degree
-poly_deg_max = 1
 
 # Distributions information to be passed to the UncertaintyQuantification Operation
 # ~ dim = len(distris)
@@ -73,79 +51,50 @@ else:
     a = np.array([-np.inf for _ in range(dim)])
     b = np.array([np.inf for _ in range(dim)])
 
-# population model definition: as a initial value problem
-def f(t, pX):
-    '''
-    ODE formulation of preyBirthRate predator & prey model.
-
-    Parameters
-    ----------
-    pX : array[2], pX[0] is the population size of predator
-
-                   pX[1] is the population size of prey
-        Mean of the distribution.
-    t : is the time
-
-    f.predatorDeathRate : death rate of predator
-    f.preyBirthRate : birth rate of prey
-    f.voracity : the voracity rate (when predator meets prey and kill it)
-    f.augmentation : the augmentation rate (when predator meets prey and a new predator growth)
-    '''
-    predatorPopulation, preyPopulation = pX
-
-    predator = (-f.predatorDeathRate + f.augmentation*preyPopulation)*predatorPopulation
-    prey = (f.preyBirthRate - f.voracity*predatorPopulation)*preyPopulation
-
-    return [predator, prey]
-
-time_points = np.linspace(0, T, NT+1)
-
-def static_var(varname, value):
-    def decorate(func):
-        setattr(func, varname, value)
-        return func
-    return decorate
-
-@static_var("counter", 0)
-def solver(voracity, Px0, f):
-    #set the parameter
-    f.preyBirthRate = sheepBirthRate
-    f.predatorDeathRate = coyoteDeathRate
-    f.voracity = voracity
-    f.augmentation = augmentation
-
-    #progress bar
-    solver.counter += 1
-    if solver.counter % 100 == 0:
-        sys.stdout.write(".")
-
-    #solves the population model
-    #u = ode.odeint(f, Px0, time_points)
-    #u = ode.solve_ivp(f, [0, T], Px0, method='BDF', t_eval=time_points)
-    u = ode.solve_ivp(f, [0, T], Px0, method='RK45', t_eval=time_points)
-
-    return u
-
+'''
 def get_solver_value(input_values):
+    assert all([not isinf(v) for v in input_values])
     # ~ voracity_sample, sheep_Px0_sample, coyote_Px0_sample = input_values
     voracity_sample, coyote_Px0_sample = input_values
     sheep_Px0_sample = sheeps_Px0
+    if voracity_sample <= 0 or coyote_Px0_sample <= 0:
+        print("negative input values")
+        return 0
     if uniform_distr:
         # Chaospy uniform distribution does not work with small values
         voracity_sample = 0.000115 + 0.00001 * voracity_sample
         coyote_Px0_sample = 35 + coyote_Px0_sample * 30
     # y contains the predator solutions and prey solutions for all time values
     y = solver(voracity_sample, [coyote_Px0_sample, sheep_Px0_sample], f).y
+    assert len(y[0]) == len(y[1]) == len(time_points), y.shape
+    return y[1][timestep_problem]
+'''
+def get_solver_value(input_values):
+    assert not uniform_distr
+    y = pp.get_solver_value2D(input_values)
     return y[1][timestep_problem]
 problem_function = FunctionCustom(get_solver_value, output_dim=1)
 
 error_operator = ErrorCalculatorSingleDimVolumeGuided()
-op = UncertaintyQuantification(None, distris, a, b, dim=dim)
+op = UncertaintyQuantificationTesting(None, distris, a, b, dim=dim)
 # ~ op = UncertaintyQuantification(problem_function, distris, a, b, dim=dim)
 # ~ pa, pb = op.get_boundaries(0.01)
 # ~ problem_function.plot(pa, pb, points_per_dim=5, filename="25.pdf")
 
-types = ("Gauss", "adaptiveTrapez", "adaptiveHO", "Fejer", "adaptiveTransBSpline", "adaptiveLagrange", "sparseGauss")
+'''
+# scipy reference solution; does not work
+pdf_function = op.get_pdf_Function()
+op.f = problem_function
+expectation_func = FunctionUQWeighted(problem_function, pdf_function)
+reference_expectation = expectation_func.getAnalyticSolutionIntegral(a, b)
+mom2_func = FunctionUQWeighted(op.get_moment_Function(2), pdf_function)
+mom2 = mom2_func.getAnalyticSolutionIntegral(a, b)
+reference_variance = mom2 - reference_expectation ** 2
+np.save("step25_2D_solutions.npy", [reference_expectation, reference_variance])
+print([reference_expectation, reference_variance])
+#'''
+
+types = ("Gauss", "adaptiveTrapez", "adaptiveHO", "Fejer", "adaptiveTransBSpline", "adaptiveLagrange", "sparseGauss", "adaptiveTransTrapez")
 typids = dict()
 for i,v in enumerate(types):
     typids[v] = i
@@ -166,11 +115,12 @@ Var_ref = Var_pX_ref[timestep_problem][1]
 def error_absolute(v, ref): return abs(ref - v)
 def error_relative(v, ref): return error_absolute(v, ref) / abs(ref)
 
-def run_test(testi, typid, exceed_evals=None):
+def run_test(testi, typid, exceed_evals=None, evals_end=None):
     problem_function_wrapped = FunctionCustom(lambda x: problem_function(x), output_dim=problem_function.output_length())
     op.f = problem_function_wrapped
 
     measure_start = time.time()
+    multiple_evals = None
     typ = types[typid]
     if typ not in ("Gauss", "Fejer", "sparseGauss"):
         do_inverse_transform = typ in ("adaptiveTransBSpline", "adaptiveTransTrapez", "adaptiveTransHO")
@@ -189,6 +139,7 @@ def run_test(testi, typid, exceed_evals=None):
             grid = GlobalTrapezoidalGrid(a_trans, b_trans, boundary=uniform_distr)
         elif typ == "adaptiveTransHO":
             grid = GlobalHighOrderGrid(a_trans, b_trans, boundary=uniform_distr, split_up=False)
+        op.set_grid(grid)
 
         if do_inverse_transform:
             # Use Integration operation
@@ -197,16 +148,20 @@ def run_test(testi, typid, exceed_evals=None):
             # ~ f_refinement.plot(np.array([0.001]*2), np.array([0.999]*2), filename="trans.pdf")
             op_integration = Integration(f_refinement, grid, dim)
             combiinstance = SpatiallyAdaptiveSingleDimensions2(a_trans, b_trans, operation=op_integration,
-                norm=2, grid=grid)
+                norm=2)
         else:
-            combiinstance = SpatiallyAdaptiveSingleDimensions2(a, b, operation=op,
-                norm=2, grid=grid)
+            combiinstance = SpatiallyAdaptiveSingleDimensions2(a, b, operation=op, norm=2)
             f_refinement = op.get_expectation_variance_Function()
 
         lmax = 3
         if typ == "Trapez":
             lmax = testi + 2
-        if exceed_evals is None or typ == "Trapez":
+        if evals_end is not None:
+            multiple_evals = dict()
+            combiinstance.performSpatiallyAdaptiv(1, lmax, f_refinement,
+                error_operator, tol=0, max_evaluations=evals_end,
+                print_output=True, solutions_storage=multiple_evals)
+        elif exceed_evals is None or typ == "Trapez":
             combiinstance.performSpatiallyAdaptiv(1, lmax, f_refinement,
                 error_operator, tol=0,
                 max_evaluations=1,
@@ -220,9 +175,10 @@ def run_test(testi, typid, exceed_evals=None):
         # ~ combiinstance.plot()
         # Calculate the gPCE using the nodes and weights from the refinement
         # ~ op.calculate_PCE(None, combiinstance)
-        E, Var = op.calculate_expectation_and_variance(combiinstance)
+        if multiple_evals is None:
+            E, Var = op.calculate_expectation_and_variance(combiinstance)
     else:
-        polys, polys_norms = cp.orth_ttr(poly_deg_max, op.distributions_joint, retall=True)
+        # ~ polys, polys_norms = cp.orth_ttr(poly_deg_max, op.distributions_joint, retall=True)
         if typ == "Gauss":
             nodes, weights = cp.generate_quadrature(testi,
                 op.distributions_joint, rule="G")
@@ -230,12 +186,16 @@ def run_test(testi, typid, exceed_evals=None):
             nodes, weights = cp.generate_quadrature(testi,
                 op.distributions_joint, rule="F", normalize=True)
         elif typ == "sparseGauss":
+            level = testi+1
+            if level > 5:
+                # normal distribution has infinite bounds
+                return np.inf
             expectations = [distr[1] for distr in distris]
             standard_deviations = [distr[2] for distr in distris]
             hgrid = GaussHermiteGrid(expectations, standard_deviations, dim)
-            # ~ combiinstance = StandardCombi(self.a, self.b, grid=grid, operation=self)
-            combiinstance = StandardCombi(a, b, grid=hgrid)
-            combiinstance.perform_combi(1, testi+1, op.get_expectation_variance_Function())
+            op.set_grid(hgrid)
+            combiinstance = StandardCombi(a, b, operation=op)
+            combiinstance.perform_combi(1, level, op.get_expectation_variance_Function())
             nodes, weights = combiinstance.get_points_and_weights()
             nodes = nodes.T
 
@@ -251,50 +211,84 @@ def run_test(testi, typid, exceed_evals=None):
         # ~ Var = reshape_result_values(var)
 
     def reshape_result_values(vals): return vals[0]
-    E = reshape_result_values(E)
-    Var = reshape_result_values(Var)
-
-    # ~ err_descs = ("E prey", "P10 prey", "P90 prey", "Var prey")
-    err_descs = ("E prey", "Var prey")
-    err_data = (
-        (E, E_ref),
-        # ~ (P10, P10_ref),
-        # ~ (P90, P90_ref),
-        (Var, Var_ref)
-    )
-    errors = []
-    for i,desc in enumerate(err_descs):
-        vals = err_data[i]
-        abs_err = error_absolute(*vals)
-        rel_err = error_relative(*vals)
-        errors.append(abs_err)
-        errors.append(rel_err)
-        print(f"{desc}: {vals[0]}, absolute error: {abs_err}, relative error: {rel_err}")
-
-    num_evals = problem_function_wrapped.get_f_dict_size()
-    result_data = (num_evals, timestep_problem, typid, errors)
-    assert len(result_data) == 4
-    assert len(errors) == 4
-
     tmpdir = os.getenv("XDG_RUNTIME_DIR")
     results_path = tmpdir + "/uqtestSD.npy"
     solutions_data = []
     if os.path.isfile(results_path):
         solutions_data = list(np.load(results_path, allow_pickle=True))
-    if all([any([d[i] != result_data[i] for i in range(3)]) for d in solutions_data]):
-        solutions_data.append(result_data)
-        np.save(results_path, solutions_data)
 
-    return num_evals
+    if multiple_evals is None:
+        E = reshape_result_values(E)
+        Var = reshape_result_values(Var)
+
+        # ~ err_descs = ("E prey", "P10 prey", "P90 prey", "Var prey")
+        err_descs = ("E prey", "Var prey")
+        err_data = (
+            (E, E_ref),
+            # ~ (P10, P10_ref),
+            # ~ (P90, P90_ref),
+            (Var, Var_ref)
+        )
+        errors = []
+        for i,desc in enumerate(err_descs):
+            vals = err_data[i]
+            abs_err = error_absolute(*vals)
+            rel_err = error_relative(*vals)
+            errors.append(abs_err)
+            errors.append(rel_err)
+            print(f"{desc}: {vals[0]}, absolute error: {abs_err}, relative error: {rel_err}")
+
+        num_evals = problem_function_wrapped.get_f_dict_size()
+        result_data = (num_evals, timestep_problem, typid, errors)
+        assert len(result_data) == 4
+        assert len(errors) == 4
+
+        if all([any([d[i] != result_data[i] for i in range(3)]) for d in solutions_data]):
+            solutions_data.append(result_data)
+            np.save(results_path, solutions_data)
+
+        return num_evals
+
+    solutions = op.calculate_multiple_expectation_and_variance(multiple_evals)
+    for num_evals, E, Var in solutions:
+        E = reshape_result_values(E)
+        Var = reshape_result_values(Var)
+
+        # ~ err_descs = ("E prey", "P10 prey", "P90 prey", "Var prey")
+        err_descs = ("E prey", "Var prey")
+        err_data = (
+            (E, E_ref),
+            # ~ (P10, P10_ref),
+            # ~ (P90, P90_ref),
+            (Var, Var_ref)
+        )
+        errors = []
+        for i,desc in enumerate(err_descs):
+            vals = err_data[i]
+            abs_err = error_absolute(*vals)
+            rel_err = error_relative(*vals)
+            errors.append(abs_err)
+            errors.append(rel_err)
+            print(f"{desc}: {vals[0]}, absolute error: {abs_err}, relative error: {rel_err}")
+
+        result_data = (num_evals, timestep_problem, typid, errors)
+        assert len(result_data) == 4
+        assert len(errors) == 4
+
+        if all([any([d[i] != result_data[i] for i in range(3)]) for d in solutions_data]):
+            solutions_data.append(result_data)
+    np.save(results_path, solutions_data)
+
+    return problem_function_wrapped.get_f_dict_size()
 
 
-evals_end = 900
-# ~ evals_end = 400
+# ~ evals_end = 900
+evals_end = 1200
 
 # For testing
-# ~ types = ("Gauss", "adaptiveTrapez", "adaptiveHO", "Fejer", "adaptiveTransBSpline", "adaptiveLagrange", "sparseGauss")
-# ~ skip_types = ("Fejer", "adaptiveTransBSpline", "adaptiveLagrange")
-skip_types = ("Fejer", "adaptiveTransBSpline", "adaptiveLagrange", "sparseGauss")
+# ~ types = ("Gauss", "adaptiveTrapez", "adaptiveHO", "Fejer", "adaptiveTransBSpline", "adaptiveLagrange", "sparseGauss", "adaptiveTransTrapez")
+skip_types = ("sparseGauss", "adaptiveLagrange", "adaptiveTransTrapez", "adaptiveTransBSpline", "Fejer")
+# ~ skip_types = ("Fejer", "adaptiveTransBSpline", "adaptiveLagrange", "sparseGauss")
 assert all([typ in types for typ in skip_types])
 
 for typid in reversed(range(len(types))):
@@ -305,10 +299,10 @@ for typid in reversed(range(len(types))):
         continue
     print("Calculations for", typ)
     testi = 0
-    evals_num = run_test(testi, typid)
+    evals_num = run_test(testi, typid, evals_end=evals_end)
     while evals_num < evals_end:
         testi = testi+1
         print(f"last evals: {evals_num}, testi {testi}")
-        evals_num = run_test(testi, typid, exceed_evals=evals_num)
+        evals_num = run_test(testi, typid, exceed_evals=evals_num, evals_end=evals_end)
 
 
