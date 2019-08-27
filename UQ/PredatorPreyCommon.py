4f0adb48c838e57dccddf1bc122cf4857244f1a0
FHof <ga84muv@mytum.de>
2019-08-27 16:38:53 +0200

    PredatorPrey: Copy common code to a new file

---
+++ UQ/PredatorPreyCommon.py
@@ -0,0 +1,123 @@
+'''
+Created on 27.02.2015
+
+Uncertainty quantification with the Stochastic collocation approach for a predator & prey model (Lotka & Voltera)
+
+@author: Florian Kuenzner
+'''
+
+import chaospy as cp
+import numpy as np
+import scipy.integrate as ode
+import matplotlib.pyplot as plotter
+import sys
+import time
+import os
+from math import isclose, isinf
+
+# Load spatially adaptive sparse grid related files
+sys.path.append('../src/')
+from Function import *
+from spatiallyAdaptiveSingleDimension2 import *
+from ErrorCalculator import *
+from GridOperation import *
+
+
+#predator = coyote
+#prey = sheep
+
+#initial parameters: sheep/coyote model!!
+coyoteDeathRate = 0.0005 #death rate of coyote
+sheepBirthRate = 0.005 #birth rate of sheep
+voracity = 0.00012 #the voracity rate (when a predator meets sheep and kill it) (Gefraessigkeit)
+augmentation = 0.002*voracity #the augmentation rate (when a coyote meets sheep and a new coyote growth) (Vermehrung)
+
+sheeps_Px0 = 2000 #initial population size of sheep population
+coyote_Px0 = 50 #initial population size of coyote population
+
+T = 70*365 # end of simulation
+NT = int(0.01 * T)  # number of time steps
+
+# Standard deviations
+sigma_voracity = 0.000002  # no uncertainty: 0.000000001, uncertainty: 0.000001
+sigma_sheeps_Px0 = 1
+sigma_coyote_Px0 = 5
+
+# Maximum PCE polynomial degree
+poly_deg_max = 1
+
+# population model definition: as a initial value problem
+def f(t, pX):
+    '''
+    ODE formulation of preyBirthRate predator & prey model.
+
+    Parameters
+    ----------
+    pX : array[2], pX[0] is the population size of predator
+
+                   pX[1] is the population size of prey
+        Mean of the distribution.
+    t : is the time
+
+    f.predatorDeathRate : death rate of predator
+    f.preyBirthRate : birth rate of prey
+    f.voracity : the voracity rate (when predator meets prey and kill it)
+    f.augmentation : the augmentation rate (when predator meets prey and a new predator growth)
+    '''
+    predatorPopulation, preyPopulation = pX
+
+    predator = (-f.predatorDeathRate + f.augmentation*preyPopulation)*predatorPopulation
+    prey = (f.preyBirthRate - f.voracity*predatorPopulation)*preyPopulation
+
+    return [predator, prey]
+
+time_points = np.linspace(0, T, NT+1)
+
+def static_var(varname, value):
+    def decorate(func):
+        setattr(func, varname, value)
+        return func
+    return decorate
+
+@static_var("counter", 0)
+def solver(voracity, Px0, f):
+    #set the parameter
+    f.preyBirthRate = sheepBirthRate
+    f.predatorDeathRate = coyoteDeathRate
+    f.voracity = voracity
+    f.augmentation = augmentation
+
+    #progress bar
+    solver.counter += 1
+    if solver.counter % 100 == 0:
+        sys.stdout.write(".")
+
+    #solves the population model
+    #u = ode.odeint(f, Px0, time_points)
+    #u = ode.solve_ivp(f, [0, T], Px0, method='BDF', t_eval=time_points)
+    u = ode.solve_ivp(f, [0, T], Px0, method='RK45', t_eval=time_points)
+
+    return u
+
+def get_solver_value3D(input_values):
+    assert all([not isinf(v) for v in input_values])
+    voracity_sample, sheep_Px0_sample, coyote_Px0_sample = input_values
+    if voracity_sample <= 0 or coyote_Px0_sample <= 0 or sheep_Px0_sample <= 0:
+        print("negative input values")
+        return 0
+    # y contains the predator solutions and prey solutions for all time values
+    y = solver(voracity_sample, [coyote_Px0_sample, sheep_Px0_sample], f).y
+    assert len(y[0]) == len(y[1]) == len(time_points), y.shape
+    return y
+
+def get_solver_value2D(input_values):
+    assert all([not isinf(v) for v in input_values])
+    voracity_sample, coyote_Px0_sample = input_values
+    sheep_Px0_sample = sheeps_Px0
+    if voracity_sample <= 0 or coyote_Px0_sample <= 0:
+        print("negative input values")
+        return 0
+    # y contains the predator solutions and prey solutions for all time values
+    y = solver(voracity_sample, [coyote_Px0_sample, sheep_Px0_sample], f).y
+    assert len(y[0]) == len(y[1]) == len(time_points), y.shape
+    return y
