a4ebda634732aace9c1344f8e0b72d5819e7df90
Michael Obersteiner <oberstei@in.tum.de>
2020-04-29 10:43:24 +0200

    Update spatiallyAdaptiveSingleDimension2.py
    Changed Version to the one used in SIAM-SISC paper.

--- src/spatiallyAdaptiveSingleDimension2.py
+++ src/spatiallyAdaptiveSingleDimension2.py
@@ -21,7 +21,7 @@ class NodeInfo(object):
 
 
 class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
-    def __init__(self, a: Sequence[float], b: Sequence[float], norm: int=np.inf, dim_adaptive: bool=True, version: int=3, operation: GridOperation=None, margin: float=None, rebalancing: bool=True, chebyshev_points=False, use_volume_weighting=False):
+    def __init__(self, a: Sequence[float], b: Sequence[float], norm: int=np.inf, dim_adaptive: bool=True, version: int=6, operation: GridOperation=None, margin: float=None, rebalancing: bool=True, chebyshev_points=False, use_volume_weighting=False):
         SpatiallyAdaptivBase.__init__(self, a, b, operation=operation, norm=norm)
         assert self.grid is not None
         self.grid_surplusses = self.grid #GlobalTrapezoidalGrid(a, b, boundary=boundary, modified_basis=modified_basis)
