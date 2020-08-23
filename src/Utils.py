7df72f08e5b25a76f0a049cc3601378d81a9a8d4
ga25tuj <markus-fabry@gmx.de>
2020-08-23 21:35:56 +0200

    added function to specify new logfile name/location; logfiles in separate processes are independent; added new evaluation scripts for blobs and random classes

--- src/Utils.py
+++ src/Utils.py
@@ -33,10 +33,12 @@ def get_cross_product_range_list(one_d_arrays: Sequence[Sequence[int]]) -> List[
 
 
 log_filename = 'log_sg'
+log_format = '%(asctime)s,%(msecs)d %(name)s %(levelname)s :: %(message)s'
+log_date_format = '%H:%M:%S'
 logging.basicConfig(filename=log_filename,
                     filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s :: %(message)s',
-                    datefmt='%H:%M:%S',
+                    format=log_format,
+                    datefmt=log_date_format,
                     level=logging.ERROR)
 logger = logging.getLogger('util')
 logger.setLevel(logging.DEBUG)
@@ -68,6 +70,19 @@ def log_debug(message: str = '', do_print: bool = False):
 
 def clear_log():
     open(log_filename, 'w').close()
+
+
+def change_log_file(name):
+    file = open(name, 'w+')  # create logfile if it doesn't exist
+    file.close()
+    fileh = logging.FileHandler(name, 'a')
+    formatter = logging.Formatter(log_format)
+    fileh.setFormatter(formatter)
+
+    log = logging.getLogger()  # root logger
+    for hdlr in log.handlers[:]:  # remove all old handlers
+        log.removeHandler(hdlr)
+    log.addHandler(fileh)  # set the new handler
 
 
 def time_func(print_output, message, function, *kwargs):
