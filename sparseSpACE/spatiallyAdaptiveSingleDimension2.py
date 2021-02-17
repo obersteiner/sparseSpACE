removed unused parts + fixed relative surplus calculation

sparseSpACE/spatiallyAdaptiveSingleDimension2.py
@@ -1277,7 +1277,7 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
         exponent = 1  # if not self.do_high_order else 2
         # assert (tuple(point_child) in self.f.f_dict)
         point_values = self.operation.get_point_values_component_grid_multiple(points_children, component_grid)
-        values = point_values
+        values = np.array(point_values) # avoids changes to point_values
         if len(points_left_parents) > 0:
             point_values_left_parents = self.operation.get_point_values_component_grid_multiple(points_left_parents,
                                                                                           component_grid)
@@ -1359,9 +1359,9 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
                 values[right_parents_in_grid] -= (factors_right_parents * point_values_right_parents.T).T
         widths = np.asarray([(
             self.operation.get_surplus_width(d, right_parent, left_parent)) for (left_parent, right_parent) in zip(left_parents, right_parents)]).reshape((len(children),1)) ** exponent
+        if self.use_relative_surplus and not np.all(point_values == 0): # uses a relative surplus calculation with a limiter at a factor of 10**-1 of max value -> this avoids too extreme blow ups of regions close to 0
+            values /= np.abs(point_values) + np.max(np.abs(point_values)) * 10**-1
         volumes = np.sum(factors * abs(values), axis=1) * widths
-        if self.use_relative_surplus:
-            volumes /= point_values
         if self.version == 0 or self.version == 2:
             evaluations = size_slize * len(children)  # * (1 + int(left_parent_in_grid) + int(right_parent_in_grid))
         else:
@@ -1404,14 +1404,15 @@ class SpatiallyAdaptiveSingleDimensions2(SpatiallyAdaptivBase):
                     volume = surplus_pole[:, index_child] / np.prod(self.grid.numPoints) * self.grid.numPoints[d] * self.grid.weights[d][index_child]
                     evaluations = np.prod(self.grid.numPoints) / self.grid.numPoints[d]
                 else:
-                    if True:#not self.grid_surplusses.modified_basis:
-                        volume = volumes[i]
-                        #volume2, evaluations = self.sum_up_volumes_for_point_vectorized(child_info=child_info, grid_points=grid_points, d=d, component_grid=component_grid)
-                        #if volume != volume2:
-                        #    print(volume, volume2, component_grid, child, left_parent, right_parent)
-                        #    assert False
-                    else:
-                        volume, evaluations = self.sum_up_volumes_for_point_vectorized(child_info=child_info, grid_points=grid_points, d=d, component_grid=component_grid)
+                    volume = volumes[i]
+
+                    #if True:#not self.grid_surplusses.modified_basis:
+                    #    #volume2, evaluations = self.sum_up_volumes_for_point_vectorized(child_info=child_info, grid_points=grid_points, d=d, component_grid=component_grid)
+                    #    #if volume != volume2:
+                    #    #    print(volume, volume2, component_grid, child, left_parent, right_parent)
+                    #    #    assert False
+                    #else:
+                    #    volume, evaluations = self.sum_up_volumes_for_point_vectorized(child_info=child_info, grid_points=grid_points, d=d, component_grid=component_grid)
                     assert volume is not None
 
                 k_old = 0