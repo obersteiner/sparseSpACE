44e00d41e93e16c01175c77343bb7cf929720f29
Sonja <imaginesmthcreative@gmail.com>
2021-05-11 09:46:24 +0200

    Added rudimentary comparison of performance to mock function

--- sparseSpACE/HP_Optimization.py
+++ sparseSpACE/HP_Optimization.py
@@ -4,18 +4,32 @@ print("I am working!")
 
 #Basically copied code from Tutorial_DEMachineLearning
 def perform_optimization_mock():
+	#prepare Dataset
 	samples = 500
 	dimension = 2
 	labels = 6
 	sklearn_dataset = deml.datasets.make_moons(n_samples=samples, noise=0.15)
 	data = deml.DataSet(sklearn_dataset, name='Input_Set')
+	#for storing the current best evaluation
+	best_evaluation = None
+	#best_evaluation = 0.7
+	print("original best evaluation: " + str(best_evaluation))
+	#todo: split Dataset
 	classification = deml.Classification(data, split_percentage=0.8, split_evenly=True, shuffle_data=True)
-	classification.perform_classification(masslumping=True, lambd=0.0, minimum_level=1, maximum_level=5, print_metrics=True)
+	classification.perform_classification(masslumping=True, lambd=0.0, minimum_level=1, maximum_level=5, print_metrics=True) #True
 	evaluation = classification.evaluate()
 	print("Percentage of correct mappings",evaluation["Percentage correct"])
-	# also more testing data can be added and the results can be printed immediately
+	#currently only looking at Percentage correct for evaluating
+	current_evaluation = evaluation["Percentage correct"]
+	if best_evaluation == None or current_evaluation < best_evaluation:
+		best_evaluation = current_evaluation
+		print("Best evaluation is now " + str(best_evaluation))
+	else:	print("Best evaluation is still " + str(best_evaluation))
+	
+	print("In the end, best evaluation is " + str(best_evaluation))
 
 
+	"""# also more testing data can be added and the results can be printed immediately
 	data_copy = data.copy()                                              # deepcopied
 	data_copy.scale_range((0.005, 0.995))                                # scaled
 	part0, part1 = data_copy.split_pieces(0.5)                           # split
@@ -24,7 +38,7 @@ def perform_optimization_mock():
 	data_copy.remove_labels(0.5)                                         # freed of some label assignments to samples
 	without_labels, with_labels = data_copy.split_without_labels()
 	with_labels.set_name("Test_new_data")
-	classification.test_data(with_labels, print_output=True)
+	classification.test_data(with_labels, print_output=True)"""
 
 perform_optimization_mock()
 print("I was working!")
