276c1838854e64e718dd98302a99e1a92b6ee365
Sonja <imaginesmthcreative@gmail.com>
2021-05-31 09:36:21 +0200

    Added function to evaluate specific combinations of HP

--- sparseSpACE/HP_Optimization.py
+++ sparseSpACE/HP_Optimization.py
@@ -7,8 +7,12 @@ import sparseSpACE.DEMachineLearning as deml
 samples = 500
 dimension = 2
 labels = 6
+#vllt anderes dataset ausprobieren?, vllt höhere dimensionen, bis zu dim=10. Note: moons immer 2d, aber z.B.
+#gaussian quantiles, classification.. kann man einstellen
 sklearn_dataset = deml.datasets.make_moons(n_samples=samples, noise=0.15, random_state=1)
 data = deml.DataSet(sklearn_dataset, name='Input_Set')
+data_moons = data.copy()
+data_moons.set_name('Moon_Set')
 def perform_optimization_grid_simple():
 	#sklearn_dataset = deml.datasets.make_moons(n_samples=samples, noise=0.15)
 	#data = deml.DataSet(sklearn_dataset, name='Input_Set')
@@ -18,46 +22,49 @@ def perform_optimization_grid_simple():
 	#storing the parameters of the best evaluation
 	best_lambd = None
 	best_masslump = None
+	best_min_lv = None
+	best_one_vs_others = None
 	#For Testing best evaluation: best_evaluation = 0.7
 	#print("original best evaluation: " + str(best_evaluation))
 	#todo: split Dataset
 	#original classification: classification = deml.Classification(data, split_percentage=0.8, split_evenly=True, shuffle_data=True)
 	#original settings: classification.perform_classification(masslumping=True, lambd=0.0, minimum_level=1, maximum_level=5, print_metrics=True)
 	#perform grid search for a few values of masslumping and lambd
-	for i in range (0, 11, 10):
-		for j in [True, False]:
-			classification = deml.Classification(data, split_percentage=0.8, split_evenly=True, shuffle_data=False)
-			current_lambd = i/10
-			current_masslump = j
-			print("current masslumping = " + str(current_masslump) + "; current lambd = " + str(current_lambd))
-			classification.perform_classification(masslumping=current_masslump, lambd=current_lambd, minimum_level=1, maximum_level=5, print_metrics=False)
-			current_time = classification._time_used
-			print ("current time needed = " + str(current_time))
-			evaluation = classification.evaluate()
-			print("Percentage of correct mappings",evaluation["Percentage correct"])
-			#currently only looking at Percentage correct and time needed for evaluating
-			current_evaluation = evaluation["Percentage correct"]
-			if best_evaluation == None or current_evaluation > best_evaluation or (current_evaluation == best_evaluation and (best_time == None or best_time>current_time)):
-				best_evaluation = current_evaluation
-				best_lambd = current_lambd
-				best_time = current_time
-				best_masslump = current_masslump
-				print("Best evaluation is now " + str(best_evaluation) + " with masslump = " + str (best_masslump) + ", lambd = " + str(best_lambd) + " and time = " + str(best_time))
-			else:	print("Best evaluation is still " + str(best_evaluation) + " with masslump = " + str (best_masslump) + " and lambd = " + str(best_lambd)+ " and time = " + str(best_time))
-			print ("END OF CURRENT EVALUATION \n")
-	print("In the end, best evaluation is " + str(best_evaluation)  + " with masslump = " + str (best_masslump) + " and lambd = " + str(best_lambd)+ " and time = " + str(best_time))
+	lambd_levels = 10
+	#Note: vllt besser logarithmisch, sehr kleine Werte oft besser (1/10, 1/100, 1/1000)
+	for cur_lambd in range (0, lambd_levels+1, 5):
+		#Note: lamd vor Allem sinnvoll wenn masslumping aus ist, sollte kaum unterschied machen wenn
+		for cur_massl in [True, False]:
+			for cur_min_lv in range (1, 4, 1):
+				for cur_one_vs_others in [True, False]:
+					classification = deml.Classification(data, split_percentage=0.8, split_evenly=True, shuffle_data=False)
+					print("current masslumping = " + str(cur_massl) + "; current lambd = " + str(cur_lambd/lambd_levels) + "; current min lv = " + str(cur_min_lv) + "; current one vs others = " + str(cur_one_vs_others))
+					classification.perform_classification(masslumping=cur_massl, lambd=cur_lambd/lambd_levels, minimum_level=cur_min_lv, maximum_level=5, one_vs_others=cur_one_vs_others, print_metrics=False)
+					cur_time = classification._time_used
+					print ("current time needed = " + str(cur_time))
+					evaluation = classification.evaluate()
+					print("Percentage of correct mappings",evaluation["Percentage correct"])
+					#currently only looking at Percentage correct and time needed for evaluating
+					cur_evaluation = evaluation["Percentage correct"]
+					if best_evaluation == None or cur_evaluation > best_evaluation or (cur_evaluation == best_evaluation and (best_time == None or best_time>cur_time)):
+						best_evaluation = cur_evaluation
+						best_lambd = cur_lambd
+						best_time = cur_time
+						best_masslump = cur_massl
+						best_min_lv = cur_min_lv
+						best_one_vs_others = cur_one_vs_others
+						print("Best evaluation is now " + str(best_evaluation) + " with masslump = " + str(best_masslump) + ", lambd = " + str(best_lambd) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others) + " and time = " + str(best_time))
+					else:	print("Best evaluation is still " + str(best_evaluation) + " with masslump = " + str(best_masslump) + ", lambd = " + str(best_lambd) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others) + " and time = " + str(best_time))
+					print ("END OF CURRENT EVALUATION \n")
+	print("In the end, best evaluation is " + str(best_evaluation)  + " with masslump = " + str (best_masslump) + ", lamb = " + str(best_lambd) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others) + " and time = " + str(best_time))
+
+def perform_optimization_at(cur_data, cur_lambd: float, cur_massl: bool, cur_min_lv: int, cur_one_vs_others: bool):
+	classification = deml.Classification(cur_data, split_percentage=0.8, split_evenly=True, shuffle_data=False)
+	classification.perform_classification(masslumping=cur_massl, lambd=cur_lambd, minimum_level=cur_min_lv, maximum_level=5, one_vs_others=cur_one_vs_others, print_metrics=False)
+	evaluation = classification.evaluate()
+	print("Percentage of correct mappings",evaluation["Percentage correct"])
 
 
-	"""# also more testing data can be added and the results can be printed immediately
-	data_copy = data.copy()                                              # deepcopied
-	data_copy.scale_range((0.005, 0.995))                                # scaled
-	part0, part1 = data_copy.split_pieces(0.5)                           # split
-	data_copy = part0.concatenate(part1)                                 # concatenated
-	data_copy.set_name('2nd_Set')                                        # renamed
-	data_copy.remove_labels(0.5)                                         # freed of some label assignments to samples
-	without_labels, with_labels = data_copy.split_without_labels()
-	with_labels.set_name("Test_new_data")
-	classification.test_data(with_labels, print_output=True)"""
-
-perform_optimization_grid_simple()
+perform_optimization_at(data_moons, 0.0, False, 1, False)
+#perform_optimization_grid_simple()
 #print("I was working!")
