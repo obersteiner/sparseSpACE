bb39fb6716cfce393dd7b48abd40433496391fd1
Sonja <imaginesmthcreative@gmail.com>
2021-06-15 13:14:06 +0200

    Implemented BO with naive rounding and arbitrary beta, but haven't revised yet

--- sparseSpACE/HP_Optimization.py
+++ sparseSpACE/HP_Optimization.py
@@ -59,39 +59,59 @@ def perform_evaluation_at(cur_data, cur_lambd: float, cur_massl: bool, cur_min_l
 	print("Percentage of correct mappings",evaluation["Percentage correct"])
 	return evaluation["Percentage correct"]
 
-#TODO: will perform Bayesian Optimization; Inputs are the data, amount of iterations(amt_it)
+#TODO: will perform Bayesian Optimization; Currently basically performs BO with Naive rounding and a weird beta
+#Note: HP Input of classification is (lambd:float, massl:bool, min_lv:int <4, one_vs_others:bool)
 def perform_BO_classification(data, amt_it: int, dim: int):
 	#notes how many x values currently are in the evidence set - starts with amt_HP+1
 	amt_HP = 4
 	cur_amt_x: int = amt_HP+1
+	x_ret = None
+	y_ret = None
 	print("I should do Bayesian Optimization for " + str(amt_it) + " iterations!")
 	C = create_evidence_set(data, amt_it, amt_HP)
 	C_x=C[0]
 	C_y=C[1]
-	print(C)
+	print("Evidence Set: \n" + str(C))
 	K_matr = get_cov_matrix(C_x, cur_amt_x)
 	print(K_matr)
 	#dummy value for beta
 	beta = 0.5
 	mu = lambda x: get_cov_vector(C_x, x, cur_amt_x).dot(np.linalg.inv(K_matr).dot(C_y))
 	sigma_sqrd = lambda x: k(x, x)-get_cov_vector(C_x, x, cur_amt_x).dot(np.linalg.inv(K_matr).dot(get_cov_vector(C_x, x, cur_amt_x)))
-	sigma = lambda x: math.sqrt(sigma_sqrd(x))
+	#takes sqrt of abs(sigma_sqrd) bc otherwise fmin gives an error - might be an overall warning sign tho
+	sigma = lambda x: math.sqrt(abs(sigma_sqrd(x)))
 	alpha = lambda x: mu(x)+(math.sqrt(beta))*sigma(x)
 	#negates alpha bc maximum has to be found
 	alpha_neg = lambda x: -alpha(x)
-	stupid_value=np.array([1, 1, 1, 1])
 	for i in range (0, amt_it, 1):
 		print("iteration: " + str(i))
 		beta = get_beta(i)
-		print("beta: " + str(beta) + " mu(x): " + str(mu(stupid_value)) + " sigma(x): " + str(sigma(stupid_value)))
-		print(alpha(stupid_value))
+		print("beta: " + str(beta))
 		#value that will be evaluated and added to the evidence set
-		print(fmin(alpha_neg, [0, 0, 0, 0]))
-		#new_x=fmin(alpha_neg, [0, 0, 0, 0])
-		#print("current new_x: " + str(new_x) + " with function value: " + str(alpha(new_x)))
-		cur_amt_x += 1
-		
-
+		new_x=fmin(alpha_neg, [0, 0, 0, 0]) #Note: Vielleicht kann man die Auswertung bounden, damit keine Werte weit weg vom Möglichen rauskommen
+		print("new x: " + str(new_x) + " with function value: " + str(alpha(new_x)))
+		#rounds the values of new_x to values usable as HPs - the question is what kind of rounding makes sense, or e.g if lambda should be bounded
+		lambd = new_x[0]
+		massl = math.trunc(new_x[1]) #is true if abs val > 1
+		min_lv = math.trunc(new_x[2])
+		if(min_lv<1): min_lv = 1
+		elif (min_lv>3): min_lv = 3
+		one_vs_others = math.trunc(new_x[3])
+		new_x_rd = [lambd, massl, min_lv, one_vs_others]
+		print("rounded: " + str(new_x_rd))
+		if(not check_if_in_array(new_x_rd, C_x)):
+			C_x[cur_amt_x] = new_x_rd
+			C_y[cur_amt_x] = perform_evaluation_at(data, new_x_rd[0], new_x_rd[1], new_x_rd[2], new_x_rd[3])
+			cur_amt_x += 1
+		else: 
+			print("Dead End! I am crying now")
+			break
+	for i in range(0, cur_amt_x, 1):
+		if(y_ret == None or y_ret<C_y[i]):
+			y_ret = C_y[i]
+			x_ret = C_x[i]
+	print("The Best value found in " + str(amt_it) + " iterations is " + str(y_ret) + " at " + str(x_ret))
+	return x_ret, y_ret
 #use squared exp. kernel as covariance function k, with parameters sigma_k and l_k
 sigma_k = 1
 l_k = 0.5
@@ -120,7 +140,6 @@ def get_cov_vector(ev_x, new_x, cur_length: int):
 def create_evidence_set(data, amt_it: int, dim_HP: int, is_random: bool = True):
 	x = np.zeros(shape=(amt_it+dim_HP+1, dim_HP))
 	y = np.zeros((amt_it+dim_HP+1))
-	print(x)
 	if(is_random):
 		for i in range(0, dim_HP+1, 1):
 			new_x = create_random_x()
@@ -128,8 +147,7 @@ def create_evidence_set(data, amt_it: int, dim_HP: int, is_random: bool = True):
 				new_x = create_random_x()
 			x[i] = new_x
 			#evaluating takes quite some time, especially for min_lv>1
-			y[i] = 0.97 #hard-coded y for faster testing of the rest
-#perform_evaluation_at(data, new_x[0], new_x[1], new_x[2], new_x[3])
+			y[i] = perform_evaluation_at(data, new_x[0], new_x[1], new_x[2], new_x[3])
 	#One could hard-code non-random values for testing
 	return x, y
 
@@ -156,17 +174,6 @@ def check_if_in_array(x, array):
 def get_beta(cur_it: int):
 	beta = cur_it/2
 	return beta
-
-#Created newer, better method above, but this may be helpful for the hardcoding stuff
-def get_init_evidence(data, dim: int, amt_it: int):
-	#hard-coded x-values. Order:[lambd: float, massl:bool, min_lv: 0<int<5, one_vs_others: bool]
-	#But: all values as float, rounded as neccessary. Bool: 0=False
-	#TODO: x-values should eventually be random, and 1+dim(=amt of Parameters) many
-	x = np.array([[0.1, 1, 1, 1], [1.1, 0, 3, 1], [0.005, 1, 2, 0], [2.0, 0, 1, 0], [0.00001, 1, 1, 1]])
-	#y-values should be evaluation at x
-	y = np.array([0.1, 0.2, 0.3, 0.4, 0.5])
-	return x, y
-
 
 #Basically copied code from Tutorial_DEMachineLearning
 #prepare Dataset
