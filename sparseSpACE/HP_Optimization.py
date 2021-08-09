9b44c37469d9b6a8171666261d6d0319b795d4d3
Sonja <imaginesmthcreative@gmail.com>
2021-08-09 19:48:10 +0200

    Save before making big changes to make code more universally usable

--- sparseSpACE/HP_Optimization.py
+++ sparseSpACE/HP_Optimization.py
@@ -7,50 +7,47 @@ from scipy.optimize import fmin
 from scipy.optimize import minimize
 
 #performs Grid Optimization
-def perform_GO_classification(data):
+def perform_GO_classification(data, lambd_levels):
 	#sklearn_dataset = deml.datasets.make_moons(n_samples=samples, noise=0.15)
 	#data = deml.DataSet(sklearn_dataset, name='Input_Set')
 	#for storing the current best evaluation and time
 	best_evaluation = None
-	best_time = None
+	#best_time = None
 	#storing the parameters of the best evaluation
 	best_lambd = None
 	best_masslump = None
 	best_min_lv = None
 	best_one_vs_others = None
-	#For Testing best evaluation: best_evaluation = 0.7
-	#print("original best evaluation: " + str(best_evaluation))
-	#todo: split Dataset
+	#todo: split Dataset??
 	#original classification: classification = deml.Classification(data, split_percentage=0.8, split_evenly=True, shuffle_data=True)
 	#original settings: classification.perform_classification(masslumping=True, lambd=0.0, minimum_level=1, maximum_level=5, print_metrics=True)
-	#perform grid search for a few values of masslumping and lambd
-	lambd_levels = 10
-	#Note: vllt besser logarithmisch, sehr kleine Werte oft besser (1/10, 1/100, 1/1000)
-	for cur_lambd in range (0, lambd_levels+1, 5):
-		#Note: lamd vor Allem sinnvoll wenn masslumping aus ist, sollte kaum unterschied machen wenn
+	#perform grid search for lambd_levels many lambd between 0 and 1 and all values for the rest
+	#Note: lambd vllt besser logarithmisch, sehr kleine Werte oft besser (1/10, 1/100, 1/1000)
+	#für for-schleife karthesisches produkt aus HO um alle tupel zu bilden und über diese tupel zu iterieren für beliebig dimensional
+	for cur_lambd_it in range (0, lambd_levels, 1):
+		cur_lambd = cur_lambd_it/lambd_levels
+		#Note: lambd vor Allem sinnvoll wenn masslumping aus ist, sollte kaum unterschied machen wenn an
 		for cur_massl in [True, False]:
-			for cur_min_lv in range (1, 4, 1):
+			for cur_min_lv in range (1, 3, 1):
 				for cur_one_vs_others in [True, False]:
-					classification = deml.Classification(data, split_percentage=0.8, split_evenly=True, shuffle_data=False)
-					print("current masslumping = " + str(cur_massl) + "; current lambd = " + str(cur_lambd/lambd_levels) + "; current min lv = " + str(cur_min_lv) + "; current one vs others = " + str(cur_one_vs_others))
-					classification.perform_classification(masslumping=cur_massl, lambd=cur_lambd/lambd_levels, minimum_level=cur_min_lv, maximum_level=5, one_vs_others=cur_one_vs_others, print_metrics=False)
-					cur_time = classification._time_used
-					print ("current time needed = " + str(cur_time))
-					evaluation = classification.evaluate()
-					print("Percentage of correct mappings",evaluation["Percentage correct"])
-					#currently only looking at Percentage correct and time needed for evaluating
-					cur_evaluation = evaluation["Percentage correct"]
-					if best_evaluation == None or cur_evaluation > best_evaluation or (cur_evaluation == best_evaluation and (best_time == None or best_time>cur_time)):
+					#use "perform evaluation at"
+					#cur_time = classification._time_used
+					#print ("current time needed = " + str(cur_time))
+					cur_evaluation = perform_evaluation_at(data, cur_lambd, cur_massl, cur_min_lv, cur_one_vs_others)
+					print("Percentage of correct mappings",cur_evaluation)
+					#if best_evaluation == None or cur_evaluation > best_evaluation or (cur_evaluation == best_evaluation and (best_time == None or best_time>cur_time)):
+					if(best_evaluation == None or cur_evaluation>best_evaluation):
 						best_evaluation = cur_evaluation
 						best_lambd = cur_lambd
-						best_time = cur_time
+						#best_time = cur_time0
 						best_masslump = cur_massl
 						best_min_lv = cur_min_lv
 						best_one_vs_others = cur_one_vs_others
-						print("Best evaluation is now " + str(best_evaluation) + " with masslump = " + str(best_masslump) + ", lambd = " + str(best_lambd) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others) + " and time = " + str(best_time))
-					else:	print("Best evaluation is still " + str(best_evaluation) + " with masslump = " + str(best_masslump) + ", lambd = " + str(best_lambd) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others) + " and time = " + str(best_time))
+						print("Best evaluation is now " + str(best_evaluation) + " with lambd = " + str(best_lambd) + ", masslump = " + str(best_masslump) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others)) #+ " and time = " + str(best_time))
+					else:	print("Best evaluation is still " + str(best_evaluation) + " with lambd = " + str(best_lambd) + ", masslump = " + str(best_masslump) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others)) #+ " and time = " + str(best_time))
 					print ("END OF CURRENT EVALUATION \n")
-	print("In the end, best evaluation is " + str(best_evaluation)  + " with masslump = " + str (best_masslump) + ", lamb = " + str(best_lambd) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others) + " and time = " + str(best_time))
+	print("In the end, best evaluation is " + str(best_evaluation)  + " with lambd = " + str(best_lambd) + ", masslump = " + str(best_masslump) + ", min_level = " + str(best_min_lv) + ", one_vs_others = " + str(best_one_vs_others))
+	#+ " and time = " + str(best_time))
 
 #performs random optimization
 def perform_RO_classification(data, amt_it: int, dim: int):
@@ -70,13 +67,16 @@ def perform_evaluation_at(cur_data, cur_lambd: float, cur_massl: bool, cur_min_l
 	classification.perform_classification(masslumping=cur_massl, lambd=cur_lambd, minimum_level=cur_min_lv, maximum_level=5, one_vs_others=cur_one_vs_others, print_metrics=False)
 	evaluation = classification.evaluate()
 	print("Percentage of correct mappings",evaluation["Percentage correct"])
+	#wenn Zeit mit reingerechnet werden soll o.ä. in dieser Funktion
 	return evaluation["Percentage correct"]
+
+classification_space = [["interval", 0, 1], ["list", 0, 1], ["list", 1, 2, 3], ["list", 0, 1]]
 
 #TODO: will perform Bayesian Optimization; Currently basically performs BO with Naive rounding and a weird beta
 #Note: HP Input of classification is (lambd:float, massl:bool, min_lv:int <4, one_vs_others:bool)
-def perform_BO_classification(data, amt_it: int, dim: int, ev_is_rand: bool = True):
+def perform_BO_classification(data, amt_it: int, function = perform_evaluation_at, space = classification_space, ev_is_rand: bool = True):
 	#notes how many x values currently are in the evidence set - starts with amt_HP+1
-	amt_HP = 4
+	amt_HP = len(space)
 	cur_amt_x: int = amt_HP+1
 	x_ret = None
 	y_ret = None
@@ -164,6 +164,8 @@ def get_cov_matrix(C_x, cur_length: int):
 		#Would have to deal with that in other methods though
 		#Important!! K is inverted. But also it's used some way in g?? Unsure what to do
 		#at least it doesn't crash rn
+		#add small values to diagonal? -> verfälscht sachen. Müsste relativ zu werten in Matrix angepasst werden
+		#z.b. 10^-15 oder so
 		K = np.identity(len(C_x))*(cur_length+10)
 	return K
 
@@ -249,14 +251,15 @@ def get_new_beta_and_l(cur_beta: float, cur_amt_x, cur_x, C_x, C_y):
 	beta_h = cur_beta+100
 	l_h = l_k+50
 	#0 if rd(x) is in ev set C_x, constant otherwise (5)
+	#penalty p(x) erhöhen wenn gleicher Wert rauskommt. z.B. immer +50 oder *2 oder anders exponentiell
 	p = lambda x: check_if_in_array(round_x_classification(x), C_x)*50
 	#gets the x value for certain l (z[1]) and beta (z[0])
-	new_x = lambda z: acq_x(z[0], z[1], C_x,C_y, cur_amt_x)
+	new_x = lambda z: acq_x(z[0], z[1], C_x, C_y, cur_amt_x)
 	#for g: x[0] is \beta+d\beta, x[1] is l. Also d\beta = \beta+d\beta-\beta
 	g = lambda x: (x[0]-cur_beta)+np.linalg.norm(cur_x-new_x(x))+p(new_x(x))
 	bounds_g = ((cur_beta, beta_h), (l_k, l_h))
 	print("About to minimize g(...)")
-	#due to numerical inaccuracies a matrix might become singular with new beta and l 
+	#due to numerical inaccuracies a matrix might become singular with new beta and l
 	#(even though it wouldn't be) mathematically - how to deal with that??
 	result = minimize(g, [1, 1], method='L-BFGS-B', bounds=bounds_g).x
 	print("New beta: " + str(result[0]) + ", New l: " + str(result[1]))
@@ -317,5 +320,7 @@ data_moons.set_name('Moon_Set')
 dataset_blobs = deml.datasets.make_blobs(n_samples=samples, n_features=dimension, centers=labels)
 data_blobs = deml.DataSet(dataset_blobs, name='Blobs_Set')
 #perform_evaluation_at(data_moons, 0.00242204, 0, 1, 0) #.98 evaluation!
-#perform_BO_classification(data_blobs, 20, dimension)
+#perform_BO_classification(data_blobs, 20)
 #perform_RO_classification(data_moons, 10, dimension)
+#perform_GO_classification(data_moons, 20)
+perform_evaluation_at(data_moons, 0.828603059876013, 0, 2, 1)
